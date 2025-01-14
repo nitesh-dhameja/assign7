d45e038c3485a50f9f46d03a3253b802a987e9cb
jatingarg619 <jatin.garg619@gmail.com>
2025-01-14 15:54:12 +0530

    new model changes

--- app/train.py
+++ app/train.py
@@ -61,7 +61,7 @@ def train_one_epoch(model, train_loader, criterion, optimizer, scaler, device, e
             
             optimizer.zero_grad()
             
-            with autocast(enabled=True, device_type='cuda'):
+            with autocast():
                 outputs = model(inputs)
                 loss = criterion(outputs, targets)
             
@@ -97,7 +97,7 @@ def validate(model, val_loader, criterion, device):
             try:
                 inputs, targets = inputs.to(device), targets.to(device)
                 
-                with autocast(enabled=True, device_type='cuda'):
+                with autocast():
                     outputs = model(inputs)
                     loss = criterion(outputs, targets)
                 
